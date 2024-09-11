843ce0ba3c6bfee78519fa505aa45d101135d47c
Matheus Ferreira <ferrmatstudio@gmail.com>
2024-09-11 14:31:13 -0300

    Primeiro commit - Sequência de Fibonacci

--- Program.cs
+++ Program.cs
@@ -4,14 +4,10 @@ class Program
 {
     static void Main()
     {
-        // Solicita ao usuário um número
-        Console.Write("Digite um número para verificar se pertence à sequência de Fibonacci: ");
+        Console.Write("Digite um número: ");
         if (int.TryParse(Console.ReadLine(), out int numero))
         {
-            // Verifica se o número pertence à sequência de Fibonacci
             bool pertence = PertenceFibonacci(numero);
-            
-            // Exibe o resultado
             if (pertence)
             {
                 Console.WriteLine($"O número {numero} pertence à sequência de Fibonacci.");
@@ -23,28 +19,22 @@ class Program
         }
         else
         {
-            Console.WriteLine("Entrada inválida. Por favor, digite um número inteiro.");
+            Console.WriteLine("Número inválido");
         }
     }
 
     static bool PertenceFibonacci(int numero)
     {
-        // Casos base
         if (numero == 0) return true;
         if (numero == 1) return true;
 
-        // Inicializa os primeiros dois números da sequência de Fibonacci
         int a = 0, b = 1;
-
-        // Gera a sequência de Fibonacci até encontrar ou exceder o número
         while (b < numero)
         {
             int temp = a + b;
             a = b;
             b = temp;
         }
-
-        // Verifica se o número é igual ao último número calculado na sequência
         return b == numero;
     }
 }
--- obj/Debug/net8.0/Fibonacci.AssemblyInfo.cs
+++ obj/Debug/net8.0/Fibonacci.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Fibonacci")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0aaf685339f769af15931f53d37f1b837eba0c18")]
 [assembly: System.Reflection.AssemblyProductAttribute("Fibonacci")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Fibonacci")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
